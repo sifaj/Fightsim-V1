e73d1773dfe8b51262f787a0bc05100318cfcb8d
sifaj <54946010+sifaj@users.noreply.github.com>
2019-10-09 14:35:50 +0200

    Inint commit
    Skapt klass med variabler och metoder, har kvar att fylla i metoder och konstruera spelet i main!

---
+++ Fightsim/Fightsim/Fighter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fightsim
+{
+    class Fighter
+    {
+        int hp = 0;
+
+        public string name = "";
+
+        Random generator = new Random(); // En random generator för att slumpa värdet för bl.a hur mycket skada fighter dealar
+
+
+        public int Attack()
+        {
+            return generator.Next(100);
+        }
+
+        public void Hurt (int amount)
+        {
+
+        }
+
+        public bool isAlive()
+        {
+
+        }
+
+        public int getHp()
+        {
+
+        }
+    }
+}
---
+++ Fightsim/Fightsim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Fightsim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO: 'So u want smoke huh?'");
+            Thread.Sleep(1000);
+            Console.WriteLine("Name yoself:");
+
+            Fighter f1 = new Fighter();
+
+            f1.name = Console.ReadLine();
+
+            Console.WriteLine("Now name yo opponent: ");
+
+            Fighter f2 = new Fighter();
+
+            f2.name = Console.ReadLine();
+        }
+    }
+}
